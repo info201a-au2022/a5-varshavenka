e8878777c4f7e0a04819fce075e980774edc5e2f
Varsha Venkatesan <varshavenkatesan@Varshas-MacBook-Pro.local>
2022-12-11 21:03:39 -0800

    draft 3

--- a5/app.R
+++ a5/app.R
@@ -7,45 +7,8 @@
 #    http://shiny.rstudio.com/
 
 
-library(shiny)
-library(dplyr)
-co2_data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
-View(co2_data)
-#Where co2_per_capita is highest
-where_highest <- function(){
-  co2_data %>%
-    filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>%
-    pull(country)
-}
-
-where_highest()
-
-
-
-#What is the average co2_per_capita across all years
-avg_co2 <- function(){
-   avgco2 <- co2_data %>%
-    summarize(avgco2 = mean(co2_per_capita, na.rm = TRUE)) %>%
-    pull(avgco2)
-  
-  return(avgco2)
-}
-
-avg_co2()
-
-#What year was co2_per_capita highest globally
-
-highest_globally <- function(){
-  high <- co2_data %>%
-   filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>%
-    pull(year)
-  return (high)
-}
-
-  
-highest_globally()
-
-
 
 # Run the application 
-shinyApp(ui = my_ui, server = my_server)
+source("my_server.R")
+source("my_ui.R")
+shinyApp(ui = ui , server = server)
---
+++ a5/my_server.R
@@ -0,0 +1,62 @@
+server <- function(input,output){
+  
+  reactive_data = reactive({
+    selected_year = as.numeric(co2_data$year)
+    return(co2_data[co2_data$year==selected_year,])
+    
+  })
+  
+  output$bar <- renderPlot({
+    
+    color <- c("green", "blue")
+    
+    our_data <- reactive_data()
+    
+    barplot(colSums(our_data[,c("CO2 Per Capita","Year")]),
+            ylab="CO2 Per Capita",
+            xlab="Year",
+            names.arg = c("CO2 Per Capita", "Year"),
+            col = color)
+  })
+}
+
+library(shiny)
+library(dplyr)
+co2_data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
+View(co2_data)
+#Where co2_per_capita is highest
+where_highest <- function(){
+  co2_data %>%
+    filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>%
+    pull(country)
+}
+
+where_highest()
+
+
+
+#What is the average co2_per_capita across all years
+avg_co2 <- function(){
+  avgco2 <- co2_data %>%
+    summarize(avgco2 = mean(co2_per_capita, na.rm = TRUE)) %>%
+    pull(avgco2)
+  
+  return(avgco2)
+}
+
+avg_co2()
+
+#What year was co2_per_capita highest globally
+
+highest_globally <- function(){
+  high <- co2_data %>%
+    filter(co2_per_capita == max(co2_per_capita, na.rm = TRUE)) %>%
+    pull(year)
+  return (high)
+}
+
+
+highest_globally()
+
+
+
